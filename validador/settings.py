c58dfee144a781dd7fc13a7aadd33eef8af3ca62
alexissaltacode <alexisguantay@saltacode.com>
2026-03-06 18:00:53 -0300

    comillas

--- validador/settings.py
+++ validador/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vx(_=i1%f0e)(6280yept6*djm$y_1j%#!liz2r9_@sb9=+g@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
